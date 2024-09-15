aad7131e9595fd6a14b36f825d8817155cf5da72
CarlosCalderon01 <Carlos.Calderon01@outlook.com>
2024-09-15 06:20:05 -0500

    delete countries test and add (dto, entity)

--- src/home/global/countries/dto/create-country.dto.ts
+++ src/home/global/countries/dto/create-country.dto.ts
@@ -1,1 +1,7 @@
-export class CreateCountryDto {}
+export class CreateCountryDto {
+  name: string;
+  iso3166_2: string;
+  iso3166_3: string;
+  num_code: number;
+  phone_code: string;
+}
--- src/home/global/countries/entities/country.entity.ts
+++ src/home/global/countries/entities/country.entity.ts
@@ -1,1 +1,10 @@
-export class Country {}
+export class Country {
+  id: number;
+  name: string;
+  iso3166_2: string;
+  iso3166_3: string;
+  num_code: number;
+  phone_code: string;
+  created_at: Date;
+  updated_at: Date;
+}
