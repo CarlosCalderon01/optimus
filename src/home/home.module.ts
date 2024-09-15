94cb3c8e70dfb42fbae14239bdf7c53076f1cb2f
CarlosCalderon01 <Carlos.Calderon01@outlook.com>
2024-09-15 06:22:40 -0500

    add language module in home

--- src/home/home.module.ts
+++ src/home/home.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CountriesModule } from './global/countries/countries.module';
+import { LanguagesModule } from './global/languages/languages.module';
 
 @Module({
-  imports: [CountriesModule],
+  imports: [CountriesModule, LanguagesModule],
+  controllers: [],
+  providers: [],
 })
 export class HomeModule {}
